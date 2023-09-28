027f225d4af4e90f124f2eb15bcc0d7f1ffdd1f1
AgusMott <motturaagustin@gmail.com>
2023-09-27 21:01:53 -0300

    first commit

---
+++ Formula1(final)/backend/src/main/java/f1/backend/controller/Controller.java
@@ -0,0 +1,21 @@
+package f1.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping()
+    public String home() {
+        return "Home";
+    }
+
+    @GetMapping("/nombre")
+    public String nombre() {
+        return "Esta parte de la aplicación se llama backend.";
+    }
+
+}
+
+
